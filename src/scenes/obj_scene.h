b30801508cfb75b3d6f3d715bad300c777923919
MrEffect <106880824+MrEffectDev@users.noreply.github.com>
2026-08-17 17:34:24 +0300

    fix: gitignore ignored models

--- src/scenes/obj_scene.h
+++ src/scenes/obj_scene.h
@@ -10,16 +10,24 @@ namespace raytracer {
         std::vector<std::shared_ptr<Shape>> Build() const override {
             std::vector<std::shared_ptr<Shape>> scene;
 
-            auto model_shapes = LoadObjModel(
+            auto cat = LoadObjModel(
                 "assets/cat_statue/model.obj",
                 Vec3(0.8, 0.7, 0.9),
                 Vec3(0.0, 0.0, 0.0),
                 MaterialType::Diffuse
             );
 
-            scene.insert(scene.end(), model_shapes.begin(), model_shapes.end());
+            scene.insert(scene.end(), cat.begin(), cat.end());
             scene.push_back(std::make_shared<Plane>(Vec3{ 0.0, -0.5, 0.0 }, Vec3{ 0.0, 1.0, 0.0 }, Vec3{ 0.8, 0.8, 0.8 }, Vec3{ 0, 0, 0 }, MaterialType::Diffuse)); // Floor
             scene.push_back(std::make_shared<Sphere>(Vec3{ 0.0, 3.0, -1.0 }, 0.5, Vec3{ 0, 0, 0 }, Vec3{ 15, 15, 15 }, MaterialType::Diffuse)); // Light Source
+            auto car = LoadObjModel(
+                "assets/car/car.obj",
+                Vec3(0.8, 0.7, 0.9),
+                Vec3(0.0, 0.0, 0.0),
+                MaterialType::Diffuse
+            );
+            scene.insert(scene.end(), car.begin(), car.end());
+
             return scene;
         }
 
